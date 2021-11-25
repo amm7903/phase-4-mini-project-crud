e8af5c4431a964e9e9bde51b80a7f96c55d3c74d
Alejandro Marcano <+>
2021-11-25 01:44:15 +0100

    completed assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,34 @@
+class SpicesController < ApplicationController
+
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spices = Spice.create(spice_params)
+        render json: spices, status: :created
+    end
+
+    
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        render json: {}
+    end
+   
+    private 
+
+
+    def spice_params
+      params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+end
